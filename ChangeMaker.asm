bc061b2a2f0f38cf01b664dd5eacb3f958058b88
snisonoff <spencer.nisonoff@stonybrook.edu>
2019-01-16 04:37:42 -0500

    First commit. The user prompt, which has not been added yet, is simulated using $t3

---
+++ ChangeMaker.asm
@@ -0,0 +1,47 @@
+.text
+addi $t0, $zero, 25 # num quarters
+addi $t1, $zero, 10 # num dimes 
+addi $t2, $zero, 5 # num nickles
+addi $t3, $zero, 26  # num change
+div $t3, $t0 # t0 represents the number of quarters
+mfhi $t3
+mflo $t0
+div $t3, $t1 #t1 represents the number of dimes
+mfhi $t3
+mflo $t1
+div $t3, $t2 #t2 represents to number of nickles
+mfhi $t3
+mflo $t2
+#s0 represents the number of pennies
+add $a0, $zero, $t0
+li $v0, 1
+syscall 
+li $v0, 4
+la $a0, String1
+syscall
+add $a0, $zero, $t1
+li $v0, 1
+syscall 
+li $v0, 4
+la $a0, String2
+syscall
+add $a0, $zero, $t2
+li $v0, 1
+syscall 
+li $v0, 4
+la $a0, String3
+syscall
+add $a0, $zero, $t3
+li $v0, 1
+syscall 
+li $v0, 4
+la $a0, String4
+syscall
+
+.data
+D: .float 26.0
+
+String1: .asciiz " quarters, "
+String2: .asciiz " dimes, "
+String3: .asciiz " nickels, and "
+String4: .asciiz " pennies"
